de7f485afdde80849ad3718019a2f677fe3ebf64
Gordon <dowspirit@gmail.com>
2024-12-01 19:06:30 -0600

    Update Files

--- crud.c
+++ crud.c
@@ -195,3 +195,13 @@ int main() {
 }
 
 // To compile gcc -o crud.exe crud.c libsqlite3.a -- You might have to install sqlite.
+// In this example, are are using the sqlite as static library.  We are not using dependencies.
+
+// You download the amalgamation files, and then compile the sqlite3.c file.
+// gcc -c sqlite3.c | We are doing this to get an object file.  It will be sqlite3.o
+
+// Then we have to make an archive.  Called libsqlite3.a
+// ar rcs libsqlite3.a sqlite3.o
+// You will then have libsqlite3.a in the directory.  
+
+// You need to move both the header file sqlite3.h and libsqlite3.a into the folder of your project.
